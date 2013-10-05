90d1ac958f88d4d555315a11fd48d348c5a6f333
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2013-10-05 22:33:28 +0600

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,3 @@
+Django Static Precompiler
+==========================
+
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+README = read('README.rst')
+CHANGES = read('CHANGES.rst')
+
+setup(
+    name="django-static-precompiler",
+    packages=find_packages(),
+    version="0.1dev",
+    author="Andrey Fedoseev",
+    author_email="andrey.fedoseev@gmail.com",
+    url="https://github.com/andreyfedoseev/django-static-precompiler",
+    description="Django template tags to compile all kinds of static files "
+                "(SASS, LESS, CoffeeScript).",
+    long_description="\n\n".join([README, CHANGES]),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    keywords=["sass", "scss", "less", "css", "coffeescript", "javascript"],
+)
