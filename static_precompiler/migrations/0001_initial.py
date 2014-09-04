ac248a4c747fbdaf586135c300d585f0aee14615
Sergey Fedoseev <fedoseev.sergey@gmail.com>
2014-09-04 21:55:04 +0600

    add django 1.7 migrations

---
+++ static_precompiler/migrations/0001_initial.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Dependency',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('source', models.CharField(max_length=255, db_index=True)),
+                ('depends_on', models.CharField(max_length=255, db_index=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AlterUniqueTogether(
+            name='dependency',
+            unique_together=set([('source', 'depends_on')]),
+        ),
+    ]
