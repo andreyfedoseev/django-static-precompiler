e3a25e5b83c146ec611684486851bab5dfccb68e
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2014-11-23 17:29:02 +0500

    Add STATIC_ROOT to test project settings

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -81,4 +81,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
