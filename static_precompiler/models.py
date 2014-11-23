39d3f605d240a8abef22107424ec1d6f76161580
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2014-11-23 18:50:22 +0500

    Add __unicode__ to Dependency model

--- static_precompiler/models.py
+++ static_precompiler/models.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from django.db import models
 
 
@@ -8,3 +9,6 @@ class Dependency(models.Model):
 
     class Meta:
         unique_together = ("source", "depends_on")
+
+    def __unicode__(self):
+        return "{0} depends on {1}".format(self.source, self.depends_on)
