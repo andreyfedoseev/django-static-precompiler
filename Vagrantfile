b3bff55bc8cf34a42814dbd6569fe08ab3edb633
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2015-02-05 19:46:34 +0500

    Add Vagrantfile

---
+++ Vagrantfile
@@ -0,0 +1,21 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  # All Vagrant configuration is done here. The most common configuration
+  # options are documented and commented below. For a complete reference,
+  # please see the online documentation at vagrantup.com.
+
+  # Every Vagrant virtual environment requires a box to build off of.
+  config.vm.box = "ubuntu/trusty64"
+
+  # The url from where the 'config.vm.box' box will be fetched if it
+  # doesn't already exist on the user's system.
+  config.vm.box_url = "https://vagrantcloud.com/ubuntu/boxes/trusty64"
+
+  config.vm.provision "shell", path: "provision.sh", privileged: false
+
+end
---
+++ provision.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+############
+# packages #
+############
+
+sudo aptitude update
+sudo aptitude install -y virtualenvwrapper python-dev ruby-dev node npm
+
+sudo gem install sass -v 3.4.9
+sudo gem install compass -v 1.0.1
+
+# Fix path to nodejs executable
+if [ ! -e /usr/local/bin/node ]; then
+    sudo ln -s /usr/bin/nodejs /usr/local/bin/node
+fi
+
+sudo npm install -g coffee-script@1.7.1
+sudo npm install -g less@1.7.4
+
+
+##############
+# virtualenv #
+##############
+
+source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+
+# Create virtualenv
+if [ ! -e ~/.virtualenvs/staticprecompiler ]; then
+    mkvirtualenv staticprecompiler
+fi
+
+echo "cd /vagrant; export DJANGO_SETTINGS_MODULE=static_precompiler.tests.django_settings" > ~/.virtualenvs/staticprecompiler/bin/postactivate
+
+workon staticprecompiler
+
+if [ ! -e ~/.pip ]; then
+    mkdir ~/.pip
+fi
+
+echo -e "[global]\ndownload_cache = ~/.cache/pip" > ~/.pip/pip.conf
+
+pip install django==1.7.4
+pip install watchdog
+pip install -e .
