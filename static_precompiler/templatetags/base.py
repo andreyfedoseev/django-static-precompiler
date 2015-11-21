4588e0a51fd828aaf0dcab496cf0c2c14018a4fe
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2015-11-21 14:24:24 -0800

    Add support for Django 1.9

--- static_precompiler/templatetags/base.py
+++ static_precompiler/templatetags/base.py
@@ -1,16 +1,21 @@
 import inspect
 
-import django.template.base
+try:
+    # Django>=1.9
+    from django.template import library
+except ImportError:
+    # Django<1.9
+    from django.template import base as library
 
 
 def container_tag(register, name=None):
+
     def dec(func):
         params, varargs, varkw, defaults = inspect.getargspec(func)
         params = params[1:]
         tag_name = name or func.__name__
 
-        class InlineCompileNode(django.template.base.TagHelperNode):
-
+        class InlineCompileNode(library.TagHelperNode):
             def __init__(self, nodelist, *args):
                 super(InlineCompileNode, self).__init__(*args)
                 self.nodelist = nodelist
@@ -22,11 +27,16 @@ def container_tag(register, name=None):
         def compile_func(parser, token):
             takes_context = True
             bits = token.split_contents()[1:]
-            args, kwargs = django.template.base.parse_bits(parser, bits, params, varargs, varkw,
-                                                           defaults, takes_context, tag_name)
+            args, kwargs = library.parse_bits(parser, bits, params, varargs, varkw,
+                                              defaults, takes_context, tag_name)
             nodelist = parser.parse(('end' + tag_name,))
             parser.delete_first_token()
-            return InlineCompileNode(nodelist, takes_context, args, kwargs)
+            try:
+                # Django<1.9
+                return InlineCompileNode(nodelist, takes_context, args, kwargs)
+            except TypeError:
+                # Django>=1.9
+                return InlineCompileNode(nodelist, func, takes_context, args, kwargs)
 
         register.tag(tag_name, compile_func)
         return func
