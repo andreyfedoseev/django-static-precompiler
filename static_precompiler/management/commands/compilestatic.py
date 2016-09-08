66282472bf0811f9ebff0e601c8688f8842dc41a
Martin Pauly <m.pauly@jumart.de>
2016-09-08 09:55:28 +0200

    Restore option_list for older Django versions

--- static_precompiler/management/commands/compilestatic.py
+++ static_precompiler/management/commands/compilestatic.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+import django
 import django.contrib.staticfiles.finders
 import django.core.files.storage
 import django.core.management.base
@@ -72,3 +73,18 @@ class Command(django.core.management.base.BaseCommand):
         if options["watch"]:
             from static_precompiler.watch import watch_dirs
             watch_dirs(scanned_dirs, verbosity)
+
+if django.VERSION < (1,8):
+    import optparse
+    Command.option_list = django.core.management.base.NoArgsCommand.option_list + (
+    optparse.make_option("--watch",
+                        action="store_true",
+                        dest="watch",
+                        default=False,
+                        help="Watch for changes and recompile if necessary."),
+    optparse.make_option("--no-initial-scan",
+                        action="store_false",
+                        dest="initial_scan",
+                        default=True,
+                        help="Skip the initial scan of watched directories in --watch mode.")
+    )
