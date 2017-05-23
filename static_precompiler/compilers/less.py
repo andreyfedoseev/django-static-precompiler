285ede408895c7d1df4fe6706512b8aa35395ac1
Jordan Reiter <jordan@aace.org>
2017-05-23 15:30:44 -0400

    Add include_path argument from lessc command

--- static_precompiler/compilers/less.py
+++ static_precompiler/compilers/less.py
@@ -21,9 +21,11 @@ class LESS(base.BaseCompiler):
     IMPORT_RE = re.compile(r"@import\s+(.+?)\s*;", re.DOTALL)
     IMPORT_ITEM_RE = re.compile(r"([\"'])(.+?)\1")
 
-    def __init__(self, executable=settings.LESS_EXECUTABLE, sourcemap_enabled=False, global_vars=None):
+    def __init__(self, executable=settings.LESS_EXECUTABLE, sourcemap_enabled=False,
+                 include_path=None, global_vars=None):
         self.executable = executable
         self.is_sourcemap_enabled = sourcemap_enabled
+        self.include_path = include_path
         self.global_vars = global_vars
         super(LESS, self).__init__()
 
@@ -47,6 +49,12 @@ class LESS(base.BaseCompiler):
         if self.is_sourcemap_enabled:
             args.extend([
                 "--source-map"
+            ])
+        if self.include_path:
+            if isinstance(self.include_path, (list, tuple)):
+                self.include_path = ';'.join(self.include_path)
+            args.extend([
+                "--include-path={}".format(self.include_path)
             ])
         if self.global_vars:
             for variable_name, variable_value in self.global_vars.items():
@@ -75,6 +83,12 @@ class LESS(base.BaseCompiler):
             self.executable,
             "-"
         ]
+        if self.include_path:
+            if isinstance(self.include_path, (list, tuple)):
+                self.include_path = ';'.join(self.include_path)
+            args.extend([
+                "--include-path={}".format(self.include_path)
+            ])
 
         return_code, out, errors = utils.run_command(args, source)
 
