ec5571ddec3228274fe03e871a518b0bcdd9fcd5
Andrey Fedoseev <andrey@andreyfedoseev.com>
2022-11-13 22:01:06 +0600

    Release 2.2

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ author = u"Andrey Fedoseev"
 # built documents.
 #
 # The short X.Y version.
-version = "2.1"
+version = "2.2"
 # The full version, including alpha/beta/rc tags.
 release = version
 
